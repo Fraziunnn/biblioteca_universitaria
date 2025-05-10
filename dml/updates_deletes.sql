2259eea46a252fb0c4f92d2881318398e5c42e46
Fraziunnn <164891876+Fraziunnn@users.noreply.github.com>
2025-05-09 22:33:52 -0300

    Update updates_deletes.sql

--- dml/updates_deletes.sql
+++ dml/updates_deletes.sql
@@ -1,1 +1,20 @@
+UPDATE aluno
+SET email = 'joao.atualizado@email.com'
+WHERE nome = 'João Silva';
 
+UPDATE multa
+SET valor = 10.00
+WHERE id_emprestimo = 4;
+
+UPDATE livro
+SET autor = 'João Pedro Souza'
+WHERE titulo = 'Algoritmos e Estruturas de Dados';
+
+DELETE FROM livro
+WHERE id_livro NOT IN (SELECT id_livro FROM emprestimo);
+
+DELETE FROM aluno
+WHERE id_aluno NOT IN (SELECT id_aluno FROM emprestimo);
+
+DELETE FROM multa
+WHERE valor = 0.00;
