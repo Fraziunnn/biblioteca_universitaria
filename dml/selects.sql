ff2cf628b3ce605d4902513dab30bc4d404ad53a
Fraziunnn <164891876+Fraziunnn@users.noreply.github.com>
2025-05-09 22:32:05 -0300

    Update selects.sql

--- dml/selects.sql
+++ dml/selects.sql
@@ -1,1 +1,12 @@
+UPDATE aluno
+SET email = 'joao.atualizado@email.com'
+WHERE nome = 'João Silva';
+
+UPDATE multa
+SET valor = 10.00
+WHERE id_emprestimo = 4;
+
+UPDATE livro
+SET autor = 'João Pedro Souza'
+WHERE titulo = 'Algoritmos e Estruturas de Dados';
 
